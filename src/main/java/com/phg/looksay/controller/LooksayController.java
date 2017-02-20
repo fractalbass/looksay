05b173403e94b4bd31983bf99f153136400e60e6
Miles Porter <mporter@paintedharmony.com>
2017-02-19 19:07:07 -0600

    Forcing http 1.0?

--- src/main/java/com/phg/looksay/controller/LooksayController.java
+++ src/main/java/com/phg/looksay/controller/LooksayController.java
@@ -4,6 +4,7 @@ import com.phg.looksay.service.LooksayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 /**
  * Created by milesporter on 5/13/16.
  */
-@RestController
+@Controller
 public class LooksayController {
 
     @Autowired
